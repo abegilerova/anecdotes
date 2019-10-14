39120d8de73ef111f8c25044b80be4bc8ae9610b
abegilerova <abegilerova@gmail.com>
2019-10-14 01:51:02 -0400

    ancdotes complete

--- src/index.js
+++ src/index.js
@@ -2,58 +2,56 @@ import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 import NextAnecdote from './components/NextAnecdote';
 import Vote from './components/Vote';
+import { type } from 'os';
 
 const App = (props) => {
+    const arr = props.anecdotes;
     const [selected, setSelected] = useState(0);
-    const [voteCount, setVoteCount] = useState(new Array(props.anecdotes.length).fill(0));
+    const [voteCount, setVoteCount] = useState(new Array(arr.length).fill(0));
 
-    console.log(typeof voteCount);
-    console.log(voteCount.length);
+    // console.log(typeof voteCount);
+    // console.log(voteCount.length);
 
     function randomIntFromInterval(min, max) { // min and max included 
         const result = Math.floor(Math.random() * (max - min + 1) + min);
-        console.log('random', result)
-        console.log('type', typeof result)
-
+        // console.log('random', result)
+        // console.log('type', typeof result)
         return result;
     }
 
     const generateNextAnecdote = (value) => {
-
         setSelected(value);
-
     }
 
     const updateVoteCount = (value) => {
-        const copy = { ...voteCount };
+        const copy = [...voteCount];
+        // console.log("copy");
+        // console.log(Array.isArray(copy));
+        // console.log(typeof copy);
         copy[value] += 1;
-
-        setVoteCount(copy);
-
-
+        console.log('math max', Math.max(...voteCount))
+        return () => {
+            setVoteCount(copy);
+        }
 
     }
 
-
-
-
-    console.log(voteCount);
-
-
+    // console.log("last")
+    // console.log(Array.isArray(voteCount));
     return (
         <div>
             <h1>Anecdote of the day</h1>
             {props.anecdotes[selected]}
             <div>
-                <Vote handleVote={updateVoteCount(selected)} numberOfVotes={voteCount[selected]} />
+                <Vote handleVote={updateVoteCount(selected)} numberOfVotes={updateVoteCount[selected]} />
                 <NextAnecdote handleClick={() => generateNextAnecdote(randomIntFromInterval(0, anecdotes.length - 1))} />
             </div>
             <h1>Anecdote with most votes</h1>
-            <div> {props.anecdotes[voteCount.indexOf(Math.max(...voteCount))]} </div>
+            <div>{arr[voteCount.indexOf(Math.max(...voteCount))]}</div>
+
         </div>
     )
 }
-
 
 const anecdotes = [
     'If it hurts, do it more often',
